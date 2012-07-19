d59dec1795114609caf18bf30f9303ac41f5718c
Soumya Deb <deb@nexvend.com>
2012-07-19 05:46:20 +0530

    Implement menu on-click event handler

--- nvfx.js
+++ nvfx.js
@@ -19,13 +19,21 @@ nvc.titles = {									///// Storage for ALL the titles
 	"3"	:	"Where we are..."
 }
 nvc.resetTitle = function () {
-	nvc.switchTitle( nvc.title );					/// Read & set default title
+	nvc.setTitle( nvc.title );						/// Read & set default title
 }
-nvc.switchTitle = function ( title ) {
+nvc.setTitle = function ( title ) {
 	if ( title === $("#title").html() ) return;		// Don't change if it's same
 	$("#title").hide().html( title ).fadeIn(200);	// if not, then change title
 }
+nvc.setArticle = function ( id ) {
+	$("article").hide();
+	$("article#" + id).fadeIn();
+}
 //		========================================================================
+
+//	-------------------------------------------------- Initiate DOM manipulation
+nvc.setArticle(1);
+//	============================================================================
 
 //	------------------------------------------------------------- Event Listenrs
 $(".switch").each(function () {
@@ -33,7 +41,16 @@ $(".switch").each(function () {
 	,	index = $this.data("title")		//
 	,	title = nvc.titles[ index ];	//// Pre-evaluation for faster-execution
 	$this.hover( function () {			// Hover catches both MouseEnter & Leave
-		nvc.switchTitle( title );			//////// MouseEnter handler, wraped
+		nvc.setTitle( title );				// MouseEnter handler, wraped method
 	},	nvc.resetTitle );					/// MouseLeave handler, function ref
+});
+$(".switch").on("click", function () {
+	var index = $(this).data("title");		/// Read the title value of the menu
+	nvc.title = nvc.titles[ index ];		///// Change the default title value
+	nvc.setTitle( nvc.title );							/// First, set the title
+	nvc.setArticle( index );							/// Then set the article
+
+	$(".switch.active").removeClass("active");	//
+	$(this).addClass("active");					//// Then apply the active-class
 });
 //	============================================================================
